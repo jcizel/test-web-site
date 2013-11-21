85ef3f6c095256c423fa952290a291cbfb786c9e
jcizel <jcizel@gmail.com>
2013-11-21 20:05:47 +0000

    add node app

---
+++ web.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+var content = fs.readFileSync("index.html")
+
+
+app.use(express.static(__dirname + '/full-slider'));
+
+app.get('/', function(request, response) {
+  response.send(content.toString());
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
