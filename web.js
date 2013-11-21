73361c74f88b3b84e39a460d11153eaa7b701c55
jcizel <jcizel@gmail.com>
2013-11-21 23:00:04 +0000

    Added web.js changes.

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express(express.logger())
+// var app = express.createServer(express.logger());
 var content = fs.readFileSync("index.html")
 
 
-app.use(express.static(__dirname + '/full-slider'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
   response.send(content.toString());
